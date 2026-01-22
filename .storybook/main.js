3aee27146755caca87050a56b3e7061d5c5f0ece
tamylaa <tamylatrading@gmail.com>
2026-01-22 14:45:10 +0530

    chore(storybook): add babel-loader for story files to transpile TSX/JSX in build

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-essentials'],
@@ -5,5 +7,23 @@ module.exports = {
   staticDirs: ['../public'],
   core: {
     builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config) => {
+    // Ensure story files (.stories.*) are transpiled (JSX/TSX) by babel-loader
+    config.module.rules.push({
+      test: /\.stories\.(js|jsx|ts|tsx)$/,
+      include: path.resolve(__dirname, '../src'),
+      use: [{
+        loader: require.resolve('babel-loader'),
+        options: {
+          presets: [
+            ['@babel/preset-env', { targets: 'defaults' }],
+            ['@babel/preset-react', { runtime: 'automatic' }],
+            '@babel/preset-typescript'
+          ]
+        }
+      }]
+    });
+    return config;
   },
 };
